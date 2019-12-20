c359dd7a225142c4b92a2850fa0968c881a08226
arashk7 <ahmad.karambakhsh@gmail.com>
2019-12-20 17:04:39 +0800

    add logger printing

--- ABaseNode.py
+++ ABaseNode.py
@@ -8,12 +8,12 @@ class ABaseNode:
         self.__category = 'none'
         self.__params_in = {}
         self.__params_out = {}
-        self._ports_in = {}
-        self._ports_out = {}
+        self.ports_in = {}
+        self.ports_out = {}
         self.__num_params_in = 0
         self.__num_params_out = 0
-        self.__num_ports_in = 0
-        self.__num_ports_out = 0
+        self.num_ports_in_created = 0
+        self.num_ports_out_created = 0
 
     # Node ID
     @property
@@ -27,9 +27,3 @@ class ABaseNode:
             return
         else:
             self.__id = node_id
-
-    # Add Input Port
-    # def add_port_in(self, port_id):
-    #     self.__num_ports_in = self.__num_ports_in + 1
-    #     new_port = ABasePort(anc_id)
-    #     self.__ports_in[port_id] = new_port
---
+++ ALogger.py
@@ -0,0 +1,72 @@
+class MsgColors:
+    CEND = '\33[0m'
+    CBOLD = '\33[1m'
+    CITALIC = '\33[3m'
+    CURL = '\33[4m'
+    CBLINK = '\33[5m'
+    CBLINK2 = '\33[6m'
+    CSELECTED = '\33[7m'
+
+    CBLACK = '\33[30m'
+    CRED = '\33[31m'
+    CGREEN = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE = '\33[36m'
+    CWHITE = '\33[37m'
+
+    CBLACKBG = '\33[40m'
+    CREDBG = '\33[41m'
+    CGREENBG = '\33[42m'
+    CYELLOWBG = '\33[43m'
+    CBLUEBG = '\33[44m'
+    CVIOLETBG = '\33[45m'
+    CBEIGEBG = '\33[46m'
+    CWHITEBG = '\33[47m'
+
+    CGREY = '\33[90m'
+    CRED2 = '\33[91m'
+    CGREEN2 = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2 = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2 = '\33[96m'
+    CWHITE2 = '\33[97m'
+
+    CGREYBG = '\33[100m'
+    CREDBG2 = '\33[101m'
+    CGREENBG2 = '\33[102m'
+    CYELLOWBG2 = '\33[103m'
+    CBLUEBG2 = '\33[104m'
+    CVIOLETBG2 = '\33[105m'
+    CBEIGEBG2 = '\33[106m'
+    CWHITEBG2 = '\33[107m'
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    ERROR = CRED
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class ALogger:
+    def __init__(self, log_to_file=False, log_file_name='log.txt'):
+        self.log_to_file = log_to_file
+        self.log_file_name = log_file_name
+
+    def print_msg(message, msg_color=MsgColors.CWHITE):
+        print(msg_color+message+MsgColors.ENDC)
+
+    def print_info(message):
+        print(MsgColors.CBLUE2 + "Info: " + message + MsgColors.ENDC)
+
+    def print_error(message):
+        print(MsgColors.ERROR + "Error: " + message + MsgColors.ENDC)
+
+    def print_warning(message):
+        print(MsgColors.WARNING + "Warning: " + message + MsgColors.ENDC)
